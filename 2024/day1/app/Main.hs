ac70d749d58bd620a1678c34c65123241f817c93
Bautista Marelli <marellibautista@gmail.com>
2024-12-07 12:22:31 -0300

    feat: add day 1 (part 2) solution.

--- 2024/day1/app/Main.hs
+++ 2024/day1/app/Main.hs
@@ -5,6 +5,7 @@ module Main where
 import Common (Input, each)
 import Control.Arrow ((&&&))
 import Data.List (sort)
+import qualified Data.Map as M
 import Data.Maybe (fromMaybe)
 import Parse (input, match)
 
@@ -14,8 +15,16 @@ prepare = fromMaybe (error "wrong input!") . match input
 solve1 :: Input -> Int
 solve1 = sum . map abs . uncurry (zipWith (-)) . each sort
 
+frequencies :: (Ord a) => [a] -> M.Map a Int
+frequencies = M.fromListWith (+) . map (,1)
+
 solve2 :: Input -> Int
-solve2 = const 0
+solve2 = sum . score . each frequencies
+ where
+  value :: M.Map Int Int -> (Int, Int) -> Int
+  value m (k, v) = k * v * M.findWithDefault 0 k m
+  score :: (M.Map Int Int, M.Map Int Int) -> [Int]
+  score (ls, rs) = map (value ls) (M.assocs rs)
 
 main :: IO ()
 main = readFile "test/input.txt" >>= print . (solve1 &&& solve2) . prepare
