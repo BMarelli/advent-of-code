36ff414bc7156f12eb031bb7480a895e1556fa18
Bautista Marelli <marellibautista@gmail.com>
2024-12-08 15:43:48 -0300

    feat: add day 2 (part 2) solution.

--- 2024/day2/app/Main.hs
+++ 2024/day2/app/Main.hs
@@ -11,14 +11,21 @@ prepare = fromMaybe (error "wrong input!") . match parse
 isSafe :: (Level -> Level -> Bool) -> Report -> Bool
 isSafe op = all (uncurry op) . pairs
 
-countSafe :: (Level -> Level -> Bool) -> Input -> Int
-countSafe op = length . filter id . map (isSafe op)
-
 solve1 :: Input -> Int
 solve1 = uncurry (+) . (countSafe lt &&& countSafe gt)
+ where
+  countSafe :: (Level -> Level -> Bool) -> Input -> Int
+  countSafe op = length . filter id . map (isSafe op)
 
 solve2 :: Input -> Int
-solve2 = const 0
+solve2 = uncurry (+) . (countSafe lt &&& countSafe gt)
+ where
+  countSafe :: (Level -> Level -> Bool) -> Input -> Int
+  countSafe op = length . filter id . map (any (isSafe op) . dampener)
+  dampener :: Report -> [Report]
+  dampener xs = xs : [removeAt i xs | i <- [0 .. length xs - 1]]
+  removeAt :: Int -> [a] -> [a]
+  removeAt i xs = take i xs ++ drop (i + 1) xs
 
 main :: IO ()
 main = readFile "test/input.txt" >>= print . (solve1 &&& solve2) . prepare
