2465e214bc4111d3911ee1d8409dfebbdf199707
Bautista Marelli <marellibautista@gmail.com>
2024-12-19 09:29:59 -0300

    feat: add day 5 (part 2) solution.

--- 2024/day5/app/Main.hs
+++ 2024/day5/app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Common (Input (..), Rules, Update)
 import Control.Arrow ((&&&))
+import Data.List (sortBy)
 import qualified Data.Map as M
 import Data.Maybe (fromMaybe)
 import qualified Data.Set as S
@@ -10,7 +11,7 @@ import Parse (match, parse)
 prepare :: String -> Input
 prepare = fromMaybe (error "Failed to parse input") . match parse
 
-getMiddle :: [Int] -> Int
+getMiddle :: Update -> Int
 getMiddle xs = xs !! (length xs `div` 2)
 
 correct :: Rules -> Update -> Bool
@@ -23,7 +24,16 @@ solve1 :: Input -> Int
 solve1 i = sum . map getMiddle . filter (correct (_rules i)) . _updates $ i
 
 solve2 :: Input -> Int
-solve2 = const 0
+solve2 i = sum . map (getMiddle . orderIncorrect) . filter (not . correct rules) . _updates $ i
+ where
+  rules = _rules i
+  compareUpdate :: Int -> Int -> Ordering
+  compareUpdate x y
+    | S.member y (M.findWithDefault mempty x rules) = LT
+    | S.member x (M.findWithDefault mempty y rules) = GT
+    | otherwise = EQ
+  orderIncorrect :: Update -> Update
+  orderIncorrect = sortBy compareUpdate
 
 main :: IO ()
 main = readFile "test/input.txt" >>= print . (solve1 &&& solve2) . prepare
